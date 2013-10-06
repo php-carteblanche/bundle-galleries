3cc61cee1cabf4b51c2a001a3ff2ff49fa80cf7e
Piero Wbmstr <me@e-piwi.fr>
2013-10-06 13:19:03 +0200

    Cleaning option settings - upgrade to version 0.0.2

--- src/SimpleViewer/SimpleViewerBundle.php
+++ src/SimpleViewer/SimpleViewerBundle.php
@@ -9,13 +9,33 @@
 
 namespace SimpleViewer;
 
+use \CarteBlanche\CarteBlanche;
+
+use \Library\Helper\Directory as DirectoryHelper;
+
 class SimpleViewerBundle
 {
 
+    protected static $bundle_config_file = 'simpleviewer_config.ini';
+
     public function __construct()
     {
-        // The galleries web directory
-        define('_GALLERIES', 'galleries/');
+        $cfgfile = \CarteBlanche\App\Locator::locateConfig(self::$bundle_config_file);
+        if (!file_exists($cfgfile)) {
+            throw new ErrorException( 
+                sprintf('Galleries bundle configuration file not found in "%s" [%s]!', $this->getPath('config_dir'), $cfgfile)
+            );
+        }
+        $cfg = CarteBlanche::getContainer()->get('config')
+            ->load($cfgfile, true, 'galleries')
+            ->get('galleries');
+
+        $galleries_web_dir = isset($cfg['galleries_dir']) ? $cfg['galleries_dir'] : null;
+        if (!empty($galleries_web_dir)) {
+            DirectoryHelper::ensureExists(
+                DirectoryHelper::slashDirname(CarteBlanche::getPath('web_path')) . $galleries_web_dir
+            );
+        }
     }
 
 }
